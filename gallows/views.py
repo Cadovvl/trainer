e059b98b2742f73bbf668960d6c9349c5a785d94
PavelYasukevich <ptntbox@gmail.com>
2020-08-14 17:09:28 +0300

    changes to start and game pages

--- gallows/views.py
+++ gallows/views.py
@@ -1,7 +1,9 @@
-import random
-
 from django.shortcuts import render
 
 
 def start(request):
     return render(request, "gallows/gallows_start.html")
+
+
+def game(request):
+    return render(request, "gallows/gallows_game.html")
