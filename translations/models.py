f35b2e981f4565482029f5e960e1b7220d1484e4
Максим Секачёв <cement-fan@ya.ru>
2020-09-20 13:24:07 +0300

    cement-hools/add_str_in_model

--- translations/models.py
+++ translations/models.py
@@ -14,9 +14,16 @@ class Word(models.Model):
     word = models.CharField(max_length=200, db_column='word')
     lang = models.CharField(max_length=2, choices=Language.choices)
 
+    def __str__(self):
+        return f"{self.lang} - {self.word}"
+
 
 class Translation(models.Model):
     source_word = models.ForeignKey(Word, on_delete=models.PROTECT, related_name='sources')
     target_word = models.ForeignKey(Word, on_delete=models.PROTECT, related_name='targets')
 
     priority = models.IntegerField()
+
+    def __str__(self):
+        return (f"({self.source_word}) перевод ({self.target_word})" 
+        f" приоритет: {self.priority}")
